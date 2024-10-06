781d91196484484b12cc78b753f3a1b2878dec71
mobaile <13915507+mobaile@users.noreply.github.com>
2024-10-06 17:33:13 +0800

    fix port

--- app/main.py
+++ app/main.py
@@ -57,7 +57,7 @@ def db_connection(db_name):
     conn.close()
 
 ################# Redis #################
-redis_client = redis.Redis(host='localhost', port=6380, db=0)
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 redis_client.flushdb() # TECH DEBT
 caching_time = 3600*12 #Cache data for 12 hours
 
